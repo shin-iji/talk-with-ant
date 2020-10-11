b8a09cd90641be7bee1d283aec85bf8bea3c419e
bEERy <siradanai27@gmail.com>
2020-10-11 14:11:16 +0700

    Delete comment

--- functions/helper/push.js
+++ functions/helper/push.js
@@ -1,10 +1,6 @@
 const request = require("request-promise");
 
 const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
-const LINE_HEADER = {
-  "Content-Type": "application/json",
-  Authorization: `Bearer n8oGQGp/o7wCxPkhGpCdQFzO1XJdbMIYl5nb4tq5hfDy9yPivTrjKK6ytE8yiSIONUhB1gwVy30jO6PCIVhnjNORCjUcCH05txDrn1vsfZqCsq9ENIkW1bO4QjqCFeq/14j9SWV1XCIQSICxpF6BSwdB04t89/1O/w1cDnyilFU=`,
-};
 
 exports.push = (channelAccessToken, userId, msg) => {
   return request({
