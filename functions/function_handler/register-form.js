0cb567e04de40892d85c5e2e5185d8843612811d
bEERy <siradanai27@gmail.com>
2020-10-25 14:30:31 +0700

    Added form button to register

--- functions/function_handler/register-form.js
+++ functions/function_handler/register-form.js
@@ -1,19 +1,22 @@
 const db = require("../database/database");
+const { Payload } = require("dialogflow-fulfillment");
+const linePayload = require("../helper/payload");
 
 module.exports = async (agent) => {
   try {
     const courseName = agent.parameters.courseName;
-    const courseId = [];
+    let courseId;
 
     const courseRef = db.collection("Training Courses");
 
     const snapshot = await courseRef.where("courseName", "==", courseName).get();
     snapshot.forEach((doc) => {
-      courseId.push(doc.id);
+      courseId = doc.id;
     });
 
-    agent.add("สมัคร");
-    agent.add(`https://liff.line.me/1654378227-QwAzgAb0/enrollcourse?courseId=${courseId[0]}`);
+    let payloadJson = linePayload.formButton(courseName, courseId);
+    let payload = new Payload(`LINE`, payloadJson, { sendAsMessage: true });
+    agent.add(payload);
   } catch (error) {
     console.log(error);
   }
