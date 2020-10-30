e44fa60629d89e6edf32d97872dfc507ecb6c2c0
bEERy <siradanai27@gmail.com>
2020-10-30 17:43:37 +0700

    Changed route format

--- functions/api/routes/routes.js
+++ functions/api/routes/routes.js
@@ -4,13 +4,23 @@ const router = express.Router();
 const coursesController = require("../controllers/courses-controller");
 const usersController = require("../controllers/users-controller");
 
-router.get("/", coursesController.getAllCourses);
-router.get("/:id", coursesController.getCourse);
-router.post("/", coursesController.createCourse);
-router.put("/:id", coursesController.updateCourse);
-router.delete("/:id", coursesController.deleteCourse);
+router.route("/").get(coursesController.getAllCourses).post(coursesController.createCourse);
+router
+  .route("/:id")
+  .get(coursesController.getCourse)
+  .put(coursesController.updateCourse)
+  .delete(coursesController.deleteCourse);
 
-router.post("/users", usersController.createUser);
-router.post("/:id/users", usersController.createUserByCourse);
+router.route("/users").post(usersController.createUser);
+router.route("/:id/users").post(usersController.createUserByCourse);
+
+// router.get("/", coursesController.getAllCourses);
+// router.get("/:id", coursesController.getCourse);
+// router.post("/", coursesController.createCourse);
+// router.put("/:id", coursesController.updateCourse);
+// router.delete("/:id", coursesController.deleteCourse);
+
+// router.post("/users", usersController.createUser);
+// router.post("/:id/users");
 
 module.exports = router;
