2a82cabc20b35a702c1576995d93dc21fe6415d5
bEERy <siradanai27@gmail.com>
2020-12-13 20:26:36 +0700

    Added owner api

--- functions/api/routes/owner-routes.js
+++ functions/api/routes/owner-routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const ownerController = require("../controllers/owner-controller");
+const { route } = require("./routes");
 
 router.route("/verify").post(ownerController.checkVerifyId);
 
 router.route("/").get(ownerController.getAllOwner);
+
+router.route("/:id/users").get(ownerController.getAllAttend);
 
 module.exports = router;
